8f0e388048befec89fa7cb97dfd989de6ae2a87b
ajr-cabbage <alexander.rakotz@gmail.com>
2026-06-08 23:56:45 -0400

    CH5:L9 complete

---
+++ handlers.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/ajr-cabbage/chirpy/internal/database"
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errMsg struct {
+		Error string `json:"error"`
+	}
+
+	respBody := errMsg{
+		Error: msg,
+	}
+
+	errDat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error Marshaling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(errDat)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error Marshaling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.WriteHeader(200)
+	body := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, 403, "403 FORBIDDEN")
+		return
+	}
+
+	cfg.fileserverHits.Store(0)
+	err := cfg.db.ResetUsers(r.Context())
+	if err != nil {
+		respondWithError(w, 400, "Error resetting users")
+		return
+	}
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("DB reset."))
+}
+
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type userInfo struct {
+		Email string `json:"email"`
+	}
+
+	type newUserResponse struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	newUserInfo := userInfo{}
+	err := decoder.Decode(&newUserInfo)
+	if err != nil {
+		respondWithError(w, 500, "Error decoding new user JSON")
+		return
+	}
+
+	newUser, err := cfg.db.CreateUser(r.Context(), newUserInfo.Email)
+	if err != nil {
+		respondWithError(w, 400, "Error adding user to db")
+		return
+	}
+
+	newUserResp := newUserResponse{
+		ID:        newUser.ID,
+		CreatedAt: newUser.CreatedAt.Time,
+		UpdatedAt: newUser.UpdatedAt.Time,
+		Email:     newUser.Email,
+	}
+
+	respondWithJSON(w, 201, newUserResp)
+}
+
+func cleanChirpBody(chirp string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(chirp, " ")
+
+	for i, word := range words {
+		if slices.Contains(badWords, strings.ToLower(word)) {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func (cfg *apiConfig) createChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type chirpInfo struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+
+	type validChirpResponse struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserID    uuid.UUID `json:"user_id"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	newChirpInfo := chirpInfo{}
+	err := decoder.Decode(&newChirpInfo)
+	if err != nil {
+		respondWithError(w, 500, "Error decoding new chirp JSON")
+		return
+	}
+
+	if len(newChirpInfo.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	newChirpParams := database.CreateChirpParams{
+		Body:   cleanChirpBody(newChirpInfo.Body),
+		UserID: newChirpInfo.UserID,
+	}
+
+	newChirp, err := cfg.db.CreateChirp(r.Context(), newChirpParams)
+	if err != nil {
+		respondWithError(w, 400, err.Error())
+		return
+	}
+
+	chirpResp := validChirpResponse{
+		ID:        newChirp.ID,
+		CreatedAt: newChirp.CreatedAt.Time,
+		UpdatedAt: newChirp.UpdatedAt.Time,
+		Body:      newChirp.Body,
+		UserID:    newChirp.UserID,
+	}
+
+	respondWithJSON(w, 201, chirpResp)
+}
--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"slices"
-	"strings"
 	"sync/atomic"
 
 	"github.com/ajr-cabbage/chirpy/internal/database"
@@ -19,113 +15,13 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte("OK"))
-}
-
-func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/html")
-	w.WriteHeader(200)
-	body := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
-	w.Write([]byte(body))
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("Metrics reset."))
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	type errMsg struct {
-		Error string `json:"error"`
-	}
-
-	respBody := errMsg{
-		Error: msg,
-	}
-
-	errDat, err := json.Marshal(respBody)
-	if err != nil {
-		log.Printf("Error Marshaling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(errDat)
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload any) {
-	dat, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error Marshaling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(dat)
-}
-
-func cleanChirpBody(chirp string) string {
-	badWords := []string{"kerfuffle", "sharbert", "fornax"}
-	words := strings.Split(chirp, " ")
-
-	for i, word := range words {
-		if slices.Contains(badWords, strings.ToLower(word)) {
-			words[i] = "****"
-		}
-	}
-	return strings.Join(words, " ")
-}
-
-func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
-	type chirp struct {
-		Body string `json:"body"`
-	}
-
-	type validResponse struct {
-		CleanedBody string `json:"cleaned_body"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	newChirp := chirp{}
-	err := decoder.Decode(&newChirp)
-	if err != nil {
-		respondWithError(w, 500, "Error decoding chirp JSON")
-		return
-	}
-
-	if len(newChirp.Body) > 140 {
-		respondWithError(w, 400, "Chirp is too long")
-		return
-	}
-
-	validResp := validResponse{
-		CleanedBody: cleanChirpBody(newChirp.Body),
-	}
-
-	respondWithJSON(w, 200, validResp)
+	platform       string
 }
 
 func main() {
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	plat := os.Getenv("PLATFORM")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatal(err)
@@ -134,13 +30,15 @@ func main() {
 	cfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
+		platform:       plat,
 	}
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app/", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", readinessHandler)
 	mux.HandleFunc("GET /admin/metrics", cfg.hitsHandler)
 	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
-	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+	mux.HandleFunc("POST /api/users", cfg.createUserHandler)
+	mux.HandleFunc("POST /api/chirps", cfg.createChirpHandler)
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
